e31a0220bba79dd5c415c1e8ee6303bd4833d5fb
dungviettran89 <dungviettran89@gmail.com>
2019-07-29 21:39:22 +0800

    Added docker clean task

--- gradles/common.groovy
+++ gradles/common.groovy
@@ -1,3 +1,7 @@
 compileJava {
     options.compilerArgs << '-parameters'
 }
+task dockerClean(type: Exec, dependsOn: 'clean') {
+    group = 'cuatoi'
+    commandLine 'docker', 'system', 'prune', '-f'
+}
