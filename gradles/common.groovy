eefbce7a18c113ae653a8ebd6658b496ab5472dd
dungviettran89 <dungviettran89@gmail.com>
2019-08-06 21:48:27 +0800

    Change how git hook is set up and reformat

--- gradles/common.groovy
+++ gradles/common.groovy
@@ -2,19 +2,14 @@ compileJava {
     options.compilerArgs << '-parameters'
 }
 
-task preCommitHooks(type: Exec) {
-    group = 'cuatoi'
-    doFirst {
-        new File("$projectDir/.git/hooks/pre-commit").text = """#!/bin/sh
-./gradlew clean build
-"""
-    }
-    commandLine 'sh', '-c', "chmod +x $projectDir/.git/hooks/pre-commit"
-}
-init.dependsOn preCommitHooks
-wrapper.dependsOn preCommitHooks
-
 task dockerClean(type: Exec, dependsOn: 'clean') {
     group = 'cuatoi'
     commandLine 'sh', '-c', 'docker system prune -f && docker rmi $(docker image list -q)'
 }
+
+//set up git hooks
+def preCommitFile = new File("$projectDir/.git/hooks/pre-commit.sh")
+preCommitFile.text = """#!/bin/sh
+./gradlew clean build
+"""
+preCommitFile.setExecutable(true, false)
