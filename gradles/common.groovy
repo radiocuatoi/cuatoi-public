633b208245e9bd53776b66fdf2361707db8521f9
dungviettran89 <dungviettran89@gmail.com>
2019-08-06 17:17:43 +0800

    Fix issue with commit hooks

--- gradles/common.groovy
+++ gradles/common.groovy
@@ -2,13 +2,14 @@ compileJava {
     options.compilerArgs << '-parameters'
 }
 
-task preCommitHooks() {
+task preCommitHooks(type: Exec) {
     group = 'cuatoi'
     doFirst {
         new File("$projectDir/.git/hooks/pre-commit").text = """#!/bin/sh
 ./gradlew clean build
 """
     }
+    commandLine 'sh', '-c', "chmod +x $projectDir/.git/hooks/pre-commit"
 }
 init.dependsOn preCommitHooks
 wrapper.dependsOn preCommitHooks
