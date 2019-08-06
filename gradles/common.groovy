74febab9d275c0206d4b7b68915c5c0076f5da6a
dungviettran89 <dungviettran89@gmail.com>
2019-08-06 15:18:44 +0800

    Added precommit hooks

--- gradles/common.groovy
+++ gradles/common.groovy
@@ -1,6 +1,18 @@
 compileJava {
     options.compilerArgs << '-parameters'
 }
+
+task preCommitHooks() {
+    group = 'cuatoi'
+    doFirst {
+        new File("$projectDir/.git/hooks/pre-commit").text = """#!/bin/sh
+./gradlew clean build
+"""
+    }
+}
+init.dependsOn preCommitHooks
+wrapper.dependsOn preCommitHooks
+
 task dockerClean(type: Exec, dependsOn: 'clean') {
     group = 'cuatoi'
     commandLine 'sh', '-c', 'docker system prune -f && docker rmi $(docker image list -q)'
